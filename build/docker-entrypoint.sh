e8a1469ce485abcfb6844ea493dd679cb5d88497
Juan Antonio Pineda Amador <juanantpiama@gmail.com>
2025-02-10 12:06:03 +0100

    ok

---
+++ build/docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+while ! mysql -u ${DB_USER} -p${DB_PASS} -h ${DB_HOST}  -e ";" ; do
+	sleep 1
+done	
+mysql -u ${DB_USER} -p${DB_PASS} -h ${DB_HOST} ${DB} < /opt/biblioteca.sql
+sed -i '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
+apache2ctl -D FOREGROUND
